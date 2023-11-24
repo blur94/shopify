93774bf08b2db04b5b8f67d6235e968cdcd90a7c
Gilead Odo <odogilead@gmail.com>
2023-11-24 21:24:12 +0100

    Initialize the interaction workflow

---
+++ app.js
@@ -0,0 +1,19 @@
+const App =() => {
+    const accordion1 = document.getElementById("accordion1");
+    const accordionContent1 = document.getElementById("accordion-content1");
+    const accordion2 = document.getElementById("accordion2");
+    const accordionContent2 = document.getElementById("accordion-content2");
+    const accordion3 = document.getElementById("accordion3");
+    const accordionContent3 = document.getElementById("accordion-content3");
+    const accordion4 = document.getElementById("accordion4");
+    const accordionContent4 = document.getElementById("accordion-content4");
+    const accordion5 = document.getElementById("accordion5");
+    const accordionContent5 = document.getElementById("accordion-content5");
+}
+
+const handleAccordion1 = (accordion1, accordionContent1) => {}
+
+App()
+
+
+
